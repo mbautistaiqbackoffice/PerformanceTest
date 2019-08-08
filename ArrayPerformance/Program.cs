74c3eaf59469fb856a788d85d65c1d4a3de694dc
mbautista <mbautista@iqbackoffice.com>
2019-08-08 09:50:59 +0800

    Included sample data for the issue of ifgroup() validation inside loops.

--- ArrayPerformance/Program.cs
+++ ArrayPerformance/Program.cs
@@ -1,8 +1,9 @@
 ﻿namespace ArrayPerformance
 {
     using System;
-
+    using System.IO;
     using BenchmarkDotNet.Running;
+    using JUST;
 
     class Program
     {
@@ -10,11 +11,21 @@
         {
             //var summary = BenchmarkRunner.Run<PerformanceArray>();
             
-            var performanceArray = new PerformanceArray();
-            performanceArray.Test();
+            //var performanceArray = new PerformanceArray();
+            //performanceArray.Test();
+
+            //TransformNonExistingFields();
 
             Console.WriteLine("COMPLETED");
             Console.ReadLine();
+        }
+
+        static void TransformNonExistingFields()
+        {
+            string input = File.ReadAllText($@"{Directory.GetCurrentDirectory()}\Inputs\test_non_existing.json");
+            string transformer = File.ReadAllText($@"{Directory.GetCurrentDirectory()}\Transformers\test_non_existing_transformer.json");
+
+            string output = JsonTransformer.Transform(transformer, input);
         }
     }
 }
